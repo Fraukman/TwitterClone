c30c776933191d9bb618257706dc945cc37efe39
Juan Souza <juan.m.souza@gmail.com>
2020-04-24 22:19:30 -0300

    start tweetHeader

---
+++ TwitterClone/Controller/TweetController.swift
@@ -0,0 +1,78 @@
+//
+//  TweetController.swift
+//  TwitterClone
+//
+//  Created by Juan Souza on 23/04/20.
+//  Copyright © 2020 Juan Souza. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "tweetCell"
+private let headerIdentifier = "headerIdentifier"
+
+class TweetController: UICollectionViewController {
+    
+    //MARK: - Properties
+    
+    private var tweet: Tweet
+    
+    //MARK: - Lifecycle
+    
+    init(tweet: Tweet){
+        self.tweet = tweet
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureCollectionView()
+        
+        print("DEBUG: Tweet caption is \(tweet.caption)")
+    }
+    
+    //MARK: - HelperFunctions
+    
+    func configureCollectionView(){
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(TweetCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.register(TweetHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerIdentifier)
+    }
+    
+}
+
+    //MARK: - UICollectionViewDataSource/Delegate
+
+extension TweetController{
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TweetCell
+        
+        return cell
+    }
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifier, for: indexPath) as! TweetHeader
+        return header
+    }
+}
+
+    //MARK: - UICollectionViewDelegateFlowLayout
+
+extension TweetController: UICollectionViewDelegateFlowLayout{
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+            return CGSize(width: view.frame.width, height: 350)
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: view.frame.width, height: 120)
+        }
+}
---
+++ TwitterClone/View/TweetHeader.swift
@@ -0,0 +1,151 @@
+//
+//  TweetHeader.swift
+//  TwitterClone
+//
+//  Created by Juan Souza on 23/04/20.
+//  Copyright © 2020 Juan Souza. All rights reserved.
+//
+
+import UIKit
+
+class TweetHeader: UICollectionReusableView{
+    
+    //MARK: - Properties
+    
+    private lazy var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48/2
+        iv.backgroundColor = .mainBlue
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleProfileImageTapped))
+        iv.addGestureRecognizer(tap)
+        iv.isUserInteractionEnabled = true
+        return iv
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "Bruce Wayne"
+        return label
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .lightGray
+        label.text = "@Batima"
+        return label
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.numberOfLines = 0
+        label.text = "Some text caption from batima for now"
+        return label
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.text = "6:33 PM - 1/28/2020"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private lazy var optionsButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .lightGray
+        button.setImage(UIImage(named: "down_arrow_24pt"), for: .normal)
+        button.addTarget(self, action: #selector(showActionSheet), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var retweetsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2 retweets"
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    private lazy var likesLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "0 likes"
+        return label
+    }()
+    
+    private lazy var statsView: UIView = {
+       let view = UIView()
+        
+        let divider1 = UIView()
+        divider1.backgroundColor = .systemGroupedBackground
+        
+        view.addSubview(divider1)
+        divider1.anchor(top: view.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingLeft: 8, height: 1.0)
+        
+        let stack = UIStackView(arrangedSubviews: [retweetsLabel,likesLabel])
+        stack.axis = .horizontal
+        stack.spacing = 12
+        
+        view.addSubview(stack)
+        stack.centerY(inView: view)
+        stack.anchor(left: view.leftAnchor, paddingLeft: 16)
+        
+        let divider2 = UIView()
+        divider2.backgroundColor = .systemGroupedBackground
+        
+        view.addSubview(divider2)
+        divider2.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, height: 1.0)
+        
+        return view
+    }()
+    //MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let labelStack = UIStackView(arrangedSubviews: [fullnameLabel,usernameLabel])
+        labelStack.axis = .vertical
+        labelStack.spacing = -6
+        
+        let stack = UIStackView(arrangedSubviews: [profileImageView,labelStack])
+        stack.spacing = 12
+        
+        addSubview(stack)
+        stack.anchor(top: topAnchor, left: leftAnchor, paddingTop: 16, paddingLeft: 16)
+        
+        addSubview(captionLabel)
+        captionLabel.anchor(top: stack.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 20, paddingLeft: 16, paddingRight: 16)
+        
+        addSubview(dateLabel)
+        dateLabel.anchor(top: captionLabel.bottomAnchor, left: leftAnchor, paddingTop: 20, paddingLeft: 16)
+        
+        addSubview(optionsButton)
+        optionsButton.centerY(inView: stack)
+        optionsButton.anchor(right: rightAnchor, paddingRight: 8)
+        
+        addSubview(statsView)
+        statsView.anchor(top: dateLabel.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 20, height: 40)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Selectors
+    
+    @objc func handleProfileImageTapped(){
+        print("DEBUG: Go to user profile...")
+    }
+    
+    @objc func showActionSheet(){
+        print("DEBUG: Handle show action sheet...")
+    }
+    
+}
